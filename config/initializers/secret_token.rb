d6092ad372640afa52ab663cc1a6b3c0e9cd2d85
Josh Sullivan <josh@dipperstove.com>
2011-01-20 15:58:43 -0800

    first commit with basic header style'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MobileLite::Application.config.secret_token = 'cfd7782579b86cc22be611bbd3834aee12def6a07809db41da954a8ee4797633f8b30b0fce0502c01160112ab510ae7627cb1fb3260545ed007c30fc537e3685'
