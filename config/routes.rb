300308d79f0c0fc041a5c9b9b6d288e5e641bdb3
Josh Sullivan <josh@dipperstove.com>
2011-01-21 12:32:48 -0800

    created location page

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ MobileLite::Application.routes.draw do
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :welcome
 
   # Sample resource route with options:
   #   resources :products do
